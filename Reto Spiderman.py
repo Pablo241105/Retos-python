1d585c7d7e056dda4d1b82cf1aa9d93e0f062b1b
Pablo241105 <114221081+Pablo241105@users.noreply.github.com>
2022-10-08 12:54:00 +0200

    Add files via upload

---
+++ Reto Spiderman.py
@@ -0,0 +1,14 @@
+recorrido1=0
+recorrido2=0
+recorrido1=(int)(input("Dime cuanto recorre:\n"))
+recorrido2=(int)(input("Dime cuanto recorre:\n"))
+if(recorrido1>recorrido2) :
+    print("ver por el 2")
+else :
+    print("ve por el 1")
+
+if(recorrido1>0 and recorrido2>0) :
+    print("Esto recorre",recorrido2)
+if(recorrido1<0 and recorrido2<0) :
+    print("Esto recorre",recorrido2)
+
